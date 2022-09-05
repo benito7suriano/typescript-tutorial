da97f2a2152972837eb11e74018c630ef40522cc
Beno Suriano <bjsuriano@gmail.com>
2022-09-05 13:27:21 -0400

    Create form with styled input field plus submit button.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './styles.css'
 
-function InputField() {
-  return <div>InputField</div>
-}
+const InputField = () => (
+  <form className='input'>
+    <input
+      type='input'
+      name='task'
+      id='task'
+      placeholder='enter a task'
+      className='input__box'
+    />
+    <button type='submit' className='input__submit'>
+      add
+    </button>
+  </form>
+)
 
 export default InputField
