f5ba89e51d5b9716cb1fbdb7d3675c0beae31b3f
Beno Suriano <bjsuriano@gmail.com>
2022-09-06 12:08:49 -0400

    Store new todo on submit.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,13 @@ import './styles.css'
 interface Props {
   todo: string
   setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleSubmit: (e: React.FormEvent) => void
 }
 
 // equally valid:
 // const InputField:React.FC<Props> = ({todo, setTodo}) => ()
-const InputField = ({ todo, setTodo }: Props) => (
-  <form className='input'>
+const InputField = ({ todo, setTodo, handleSubmit }: Props) => (
+  <form className='input' onSubmit={handleSubmit}>
     <input
       type='input'
       name='task'
